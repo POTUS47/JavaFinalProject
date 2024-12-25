27e03a2aaf40b1c9fff42424fb8bc920d97f9109
sleep-sleep-yyy <1051095699@qq.com>
2024-12-26 02:42:15 +0800

    feat  添加收藏相关接口
    注意数据库description字段名修改
    暂时没用JWT

--- src/main/java/com/finalproject/repository/ProductRepository.java
+++ src/main/java/com/finalproject/repository/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.finalproject.repository;
 import com.finalproject.model.Buyer;
 import com.finalproject.model.Product;
+import com.finalproject.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 
+    List<Product> findByStore(Store store);
 }
