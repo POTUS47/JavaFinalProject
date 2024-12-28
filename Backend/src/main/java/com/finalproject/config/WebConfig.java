705abba7b3badd2af520c0413d6a355e90cd408a
POTUS47 <111119326+POTUS47@users.noreply.github.com>
2024-12-28 18:20:07 +0800

    Update WebConfig.java

---
+++ Backend/src/main/java/com/finalproject/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.finalproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 所有接口都支持CORS
+                .allowedOrigins("*") // 允许所有的源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                .allowedHeaders("*") // 允许所有的请求头
+                .allowCredentials(true); // 允许携带凭证（cookie）
+    }
+}
